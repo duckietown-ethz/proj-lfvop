77e4927e115cfbe4e4570d9fd9a7e51f27f1b387
lienharj <jolienhart8@gmail.com>
2019-10-24 18:25:13 +0200

    skleteon

---
+++ launch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+# YOUR CODE BELOW THIS LINE
+# ----------------------------------------------------------------------------
+# roslaunch my_lanefollowing my_lanefollowing.launch veh:=$VEHICLE_NAME
