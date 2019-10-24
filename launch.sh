58e3824cd492195f2d70d61cdac9f1b55000dd3a
paulawulkop <pwulkop@student.ethz.ch>
2019-10-24 20:20:56 +0200

    cleaned up

--- launch.sh
+++ launch.sh
@@ -4,4 +4,4 @@ set -e
 
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
-roslaunch dynamic_obstacle_avoidance my_lanefollowing.launch veh:=$VEHICLE_NAME
+roslaunch dynamic_obstacle_avoidance dynamic_obstacle_avoidance.launch veh:=$VEHICLE_NAME
