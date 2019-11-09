c97ad5207bda56805586b17cfde05ee1fe5e4238
paulawulkop <pwulkop@student.ethz.ch>
2019-11-09 17:05:51 +0100

    now cropping out yellow lane sections, running but not verfiyed. todo: crop image to consider only road

--- packages/dynamic_obstacle_avoidance/src/duckie_detection_node.py
+++ packages/dynamic_obstacle_avoidance/src/duckie_detection_node.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from copy import deepcopy
 from cv_bridge import CvBridge, CvBridgeError
-from duckietown_msgs.msg import BoolStamped, VehicleCorners, Pixel, SegmentList
+from duckietown_msgs.msg import BoolStamped, VehicleCorners, Pixel, SegmentList, Vector2D
 from geometry_msgs.msg import Point32, Point
 from mutex import mutex
 from sensor_msgs.msg import CompressedImage, Image
@@ -39,7 +39,7 @@ class DuckieDetectionNode(object):
         self.sub_switch = rospy.Subscriber("~switch", BoolStamped,
                                            self.cbSwitch, queue_size=1)
 
-        #self.sub_lane_segments = rospy.Subscriber("~seglist_filtered", SegmentList, self.laneHandling, "lane_filter", queue_size=1)
+        self.sub_lane_segments = rospy.Subscriber("~seglist_filtered", SegmentList, self.laneHandling, queue_size=1)
 
         self.pub_detection = rospy.Publisher("~detection",
                                              BoolStamped, queue_size=1)
@@ -57,8 +57,14 @@ class DuckieDetectionNode(object):
         #subscribe to /lane_filter_node/seglist_filtered and take pixels_normalized with color yellow and set to zero in mask to ignore yellow lane
 
         self.H = self.load_homography()
-        #self.Hinv = np.linalg.inv(self.H)
+        self.Hinv = np.linalg.inv(self.H)
         #self.pcm_ = PinholeCameraModel()
+        self.p0 = Pixel()
+        self.p1 = Pixel()
+        #self.lane_seg=np.array([[self.p0,self.p1]])
+        self.lane_seg=np.array([])
+        self.lane_detected = 0
+        self.lane_width_thresh =10 # erase also all yellow pixel close to lane, not used yet
 
         self.yellow_low = np.array([20,100,100]) #load from yaml file in the future
         self.yellow_high = np.array([30,255,255])
@@ -99,12 +105,19 @@ class DuckieDetectionNode(object):
         start = rospy.Time.now()
 
         hsv_img = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-        #crop image to remove parts where lane is
+
         mask = cv2.inRange(hsv_img, self.yellow_low, self.yellow_high)
+        if self.lane_detected:#set all yellow lane segments to zero in mask
+            self.lane_detected = 0
+            for i in range(self.lane_seg.shape[0]):
+                mask[self.lane_seg[i][0].u:self.lane_seg[i][1].u,self.lane_seg[i][0].v:self.lane_seg[i][1].v]=0
+                cv2.rectangle(cv_image,(self.lane_seg[i][0].u,self.lane_seg[i][0].v),(self.lane_seg[i][1].u,self.lane_seg[i][1].v),(0,0,255),2) #for debuging mark areas that have been erased from mask
+                #print "cutting out lane segment"
+
         img_cont, contours, hierarchy=cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)#threshold
-        #cv2.drawContours(image_received,contours,-1,(0,0,255),3) #for debugging
-        duckiefound=1 #debug
-        distance=2#debug
+        #cv2.drawContours(cv_image,contours,-1,(0,0,255),3) #for debugging
+        duckiefound=0 #init
+        distance=0#init
         duckie_loc_pix = Pixel()
 
         for item in range(len(contours)):
@@ -115,7 +128,7 @@ class DuckieDetectionNode(object):
                 duckie_loc_pix.u=int(x+w/2)
                 duckie_loc_pix.v=y+h
                 duckie_loc_world = self.pixel2ground(duckie_loc_pix)
-                distance = duckie_loc_world.y
+                distance = duckie_loc_world.y #currently just takes last contor, change this to biggest contor?
                 duckiefound = 1
 
 
@@ -131,9 +144,23 @@ class DuckieDetectionNode(object):
             image_msg_out = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
             self.pub_boundingbox_image.publish(image_msg_out)
 
-    def laneHandling(self,lane_segments):
-        print("subscribed to lane")
-        
+    def laneHandling(self,lane_segments_msg):
+
+        seg_points1 = Point()
+        seg_points2 = Point()
+        seg_points = np.array([seg_points1,seg_points2])
+
+        i=0
+        for s in range (len(lane_segments_msg.segments)):
+            if lane_segments_msg.segments[s].color==1: #0: white 1: yellow
+
+                self.lane_detected = 1
+                seg_points = lane_segments_msg.segments[s].points
+                self.p0=self.ground2pixel(seg_points[0])
+                self.p1=self.ground2pixel(seg_points[1])
+                self.lane_seg[i][:]=np.array([[self.p0,self.p1]])
+                i=i+1
+
 
     def pixel2ground(self,pixel):
         uv_raw = np.array([pixel.u, pixel.v])
@@ -150,6 +177,22 @@ class DuckieDetectionNode(object):
         point.y = y/z
         point.z = 0.0
         return point
+
+    def ground2pixel(self, point):
+        ground_point = np.array([point.x, point.y, 1.0])
+        image_point = np.dot(self.Hinv, ground_point)
+        image_point = image_point / image_point[2]
+
+
+        pixel = Pixel()
+        # if not self.rectified_input:
+        #     distorted_pixel = self.pcm_.project3dToPixel(image_point)
+        #     pixel.u = distorted_pixel[0]
+        #     pixel.v = distorted_pixel[1]
+        #else:
+        pixel.u = int(image_point[0])
+        pixel.v = int(image_point[1])
+        return pixel
 
     def load_homography(self):
         '''Load homography (extrinsic parameters)'''
